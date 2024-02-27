3ab16f27da271490b6fa6883ff603046f144e875
s0n <sngbango@yahoo.fr>
2024-02-27 09:32:30 +0100

    Update remote-backend.tf

--- containerization/infrastructure/Initialization/remote-backend.tf
+++ containerization/infrastructure/Initialization/remote-backend.tf
@@ -16,18 +16,18 @@ resource "azurerm_resource_group" "POC-ITT-tfstate" {
   location = "East US"
 }
 
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstatepocitt2024"
-  resource_group_name      = azurerm_resource_group.POC-ITT-tfstate.name
-  location                 = azurerm_resource_group.POC-ITT-tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_nested_items_to_be_public = false
+# resource "azurerm_storage_account" "tfstate" {
+#   name                     = "tfstatepocitt2024"
+#   resource_group_name      = azurerm_resource_group.POC-ITT-tfstate.name
+#   location                 = azurerm_resource_group.POC-ITT-tfstate.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   allow_nested_items_to_be_public = false
 
-}
+# }
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
+# resource "azurerm_storage_container" "tfstate" {
+#   name                  = "tfstate"
+#   storage_account_name  = azurerm_storage_account.tfstate.name
+#   container_access_type = "private"
+# }
