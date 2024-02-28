8c6c45365f60e561ed624692a3c86459454e678d
s0n <sngbango@yahoo.fr>
2024-02-28 14:16:52 +0100

    wip

--- containerization/infrastructure/deploy-container/provider.tf
+++ containerization/infrastructure/deploy-container/provider.tf
@@ -6,7 +6,6 @@ terraform {
     }
   }
 
-  #Before use remote backend you have to create a storage account and storage container on azure
   backend "azurerm" {
     resource_group_name  = "POCITT-Initialization"
     storage_account_name = "tfstatepocitt2024"
